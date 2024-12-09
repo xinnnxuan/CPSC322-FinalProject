6be3b8cd1fe029f7fee333c0c861e6f1db5bfce9
root <lbodenbender@zagmail.gonzaga.edu>
2024-12-09 22:19:45 +0000

    presentation

--- wine_app.py
+++ wine_app.py
@@ -70,5 +70,5 @@ def predict_rating(unseen_instance, classifier):
         return None
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    app.run(host='0.0.0.0', port=5000, debug=False) 
     # TODO when deploy app to "production", set debut=False
